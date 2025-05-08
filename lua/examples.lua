9265b6f94365228577cbc4be041face9e23f3ff4
Meron Main PC <meronb1312@gmail.com>
2025-05-09 02:58:48 +0300

    starting to build the bones

---
+++ lua/examples.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+function M.on_oil_preview()
+  ---TODO
+end
+
+function M.on_buffer_open()
+  ---TODO
+end
+
+function M.under_cursor()
+  ---TODO
+end
+
+return M
---
+++ lua/terminal.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+function M.draw_image(img, offset)
+  ---TODO
+end
+
+function M.clean_images()
+  ---TODO, kitty should have seperate logic
+end
+
+return M
